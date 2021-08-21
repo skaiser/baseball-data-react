3aaf4963c58b774f4569cd78b3ad2e4a15bd70c0
Glitch (emerald-pale-budget) <none>
2021-08-21 18:42:55 +0000

    Some refactoring to split compnents and start hooking up <select>s

---
+++ src/components/filters.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export class Filters extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return "";
+  }
+}
---
+++ src/components/welcome.jsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function Welcome(props) {
+  return <h1>Go {props.name}!</h1>;
+}
